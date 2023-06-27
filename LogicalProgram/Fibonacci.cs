66ce4b600e95074a4d3d287e46f09da985eb847c
HEMANTSAINI251997 <hemantsaini2525@gmail.com>
2023-06-27 16:21:39 +0530

    [HEMANT]Add: This is a program for Fibonacci Series

---
+++ LogicalProgram/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class FibonacciSeries
+    {
+        public void Checkfibonacci()
+        {
+
+            Console.WriteLine("Enter the number of terms to generate in the series ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Fibonacci Series:");
+            int firstTerm = 0;
+            int secondTerm = 1;
+
+            Console.Write(firstTerm + " " + secondTerm);
+
+            for (int val = 2; val < num; val++)
+            {
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+                Console.Write(" " + nextTerm);
+            }
+        }
+    }
+
+}
---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                bool flag = true;
+                while (flag)
+                {
+                    Console.WriteLine("\n" + "1.FibonacciSeries" + "\n");
+                    Console.WriteLine("\n" + "2.For EXIT" + "\n");
+                    Console.WriteLine("Enter your choice: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            FibonacciSeries series = new FibonacciSeries();
+                            series.Checkfibonacci();
+                            break;
+                        case 2:
+                            flag = false;
+                            break;
+                        default:
+                            Console.WriteLine("Please enter correct Choice");
+                            break;
+
+                    }
+                }
+            }
+
+        }
+    }
+
+}
